f008da1d502f37cee5340d37cead6b07397a24f7
ashok <ashokmommyguy@gmail.com>
2025-09-16 17:55:15 +0530

    developed get-interested properties end point and functionalities

--- controller/interested_controller.py
+++ controller/interested_controller.py
@@ -1,5 +1,8 @@
-from models.interested_model import InterestedModel
+from datetime import datetime
 
+from models.interested_model import InterestedModel
+from models.properties_model import Property
+from bson import ObjectId
 async def push_interested_property(user_id, property_id):
     try:
         interested = InterestedModel(user_id=user_id,properties=[property_id])
@@ -25,3 +28,29 @@ async def push_interested_property(user_id, property_id):
         return {"status_code": 500, "message": "Internal server error"}
 
 
+async def get_interested_property(user_id):
+    #check if the user has liked properries or not
+    try:
+        result_list=list()
+        interested = InterestedModel(user_id=user_id,properties=[""])
+        is_user_interested_property = await interested.find_one({"user_id": user_id})
+        print(is_user_interested_property)
+        if is_user_interested_property:
+            property_list = is_user_interested_property.properties
+            object_ids = [ObjectId(p) for p in property_list]
+            properties = await Property.find({"_id": {"$in": object_ids}}).to_list()
+            for prop in properties:
+                pro = prop.model_dump(by_alias=True)  # convert Beanie Document → dict
+                pro["_id"] = str(pro["_id"])
+                # Safely format dates
+                if isinstance(pro.get("auction_start_date"), datetime):
+                    pro["auction_start_date"] = pro["auction_start_date"].strftime("%d-%m-%Y %I:%M %p")
+                if isinstance(pro.get("auction_end_date"), datetime):
+                    pro["auction_end_date"] = pro["auction_end_date"].strftime("%d-%m-%Y %I:%M %p")
+                result_list.append(pro)
+            return {"status_code": 200, "message":{"data":result_list}}
+        else:
+            return {"status_code":200, "message": "No property is added as interested property"}
+    except Exception as e:
+        print("Error happened at get_interested_property", e)
+        return {"status_code": 500, "message": "Internal server error"}
--- routes/interested.py
+++ routes/interested.py
@@ -2,7 +2,7 @@ import os
 
 from fastapi import APIRouter,Request
 from starlette.responses import JSONResponse
-from controller.interested_controller import push_interested_property
+from controller.interested_controller import push_interested_property,get_interested_property
 from utility.security import verify_jwt_token
 from dotenv import load_dotenv
 load_dotenv()
@@ -24,6 +24,16 @@ async def interested_property(request:Request):
         return JSONResponse(content=result.get("message"),status_code=result.get("status_code"))
 
 
-
+@interested_route.get("/interested-properties")
+async def interested_users(request:Request):
+    jwt_token = request.headers.get("Authorization")
+    if jwt_token is None:
+        return JSONResponse({"message": "No JWT token provided"}, status_code=401)
+    is_token_valid = verify_jwt_token(token=jwt_token, secret_key=os.getenv("SECRET"))
+    if is_token_valid is None:
+        return JSONResponse({"message": "Invalid JWT token"}, status_code=401)
+    else:
+        result  = await get_interested_property(is_token_valid.get('user_id'))
+        return JSONResponse(status_code=result.get("status_code"),content=result.get("message"))
 
 
